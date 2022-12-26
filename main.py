da4aa371fd369f07e1a3e8a045a1843a06a9638c
lukaszrychlowski <rychlowski@agh.edu.pl>
2022-12-27 00:40:54 +0100

    add plot items

--- main.py
+++ main.py
@@ -13,23 +13,27 @@ class MainWindow(QtWidgets.QWidget):
         self.show()
 
     def interface(self):
-        #quit button
-        btn = QtWidgets.QPushButton('Quit', self)
-        btn.clicked.connect(self.close_app)
 
         #plot window
         plot = pg.PlotWidget(self)
+        plot.setBackground('w')
+        x = [0,1,2,3,4,5,6,7,8,9]
+        y = [0,1,4,9,16,25,36,49,64,81]
+        plot.plot(x,y, pen=None, symbol='o')
+
+        #quit button
+        btn = QtWidgets.QPushButton('Quit', self)
+        btn.clicked.connect(self.close_app)
         
         #layout
         layout = QtWidgets.QVBoxLayout()
         self.setLayout(layout)
-        layout.addWidget(btn)
         layout.addWidget(plot)
+        layout.addWidget(btn)
 
     def close_app(self):
         sys.exit('app closed!')
-
-
+    
 def main():
     app = QtWidgets.QApplication(sys.argv)
     main = MainWindow()
